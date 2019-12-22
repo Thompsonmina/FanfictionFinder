caa1adf80b67b978a98783e6a39b8a05b0b0f24c
Thompson inc <froshiyke@gmail.com>
2019-12-22 22:29:07 +0100

    modified the script, added comments and made it more dynamic

--- fanfiction.py
+++ fanfiction.py
@@ -2,10 +2,11 @@
 
 import requests
 from bs4 import BeautifulSoup
-import pprint, re, time
+import re
 
 
 def extractStoryDetails(storyTitle, storyFooter):
+	"""Gets all the stories and thier links from a page"""
 	stories = []
 	for i in range(len(storyTitle)):
 		storyTitlesDetail = (storyTitle[i].get_text(), storyTitle[i].get("href"))
@@ -14,6 +15,7 @@ def extractStoryDetails(storyTitle, storyFooter):
 	return stories
 
 def getReviewNumbers(singleStoryFooter):
+	"""Gets the number of reviews from the html using regex"""
 	words = singleStoryFooter.get_text()
 	review = re.compile(r"Reviews: \d+").search(words)
 	if review:
@@ -21,25 +23,49 @@ def getReviewNumbers(singleStoryFooter):
 		return int(reviewNum)
 	else: return 0
 
-def sortByReview(num, stories):
-	sortedstories = filter(lambda s: s["reviews"] >= num, stories)
+def filterByReview(minReviewNum, stories):
+	"""filters through all the stories and returns an abridged list based on the minimum reviews given"""
+	sortedstories = filter(lambda s: s["reviews"] >= minReviewNum, stories)
 	return [x for x in sortedstories]
 
-def main(num):
-	megaList2 = []
-	for i in range(1, num):
-		pageData = requests.get(f"https://www.fanfiction.net/book/Harry-Potter/?&srt=1&r=103&p={i}")
+def compileFics(title, numOfPages,minNumOfReviews):
+	"""gets the html, processes it into a giant list  and writes that list to a txt file """
+	megaList = []
+	for i in range(1, numOfPages):
+		pageData = requests.get(f"https://www.fanfiction.net/book/{title}/?&srt=1&r=10&p={i}")
 		pageHtml = BeautifulSoup(pageData.text, "html.parser")
 		storyFooter = pageHtml.select(".z-padtop2")
 		storyTitles = pageHtml.select(".stitle")
+		
+		if not storyFooter:
+			return False
 
-		body = extractStoryDetails2(storyTitles, storyFooter)
-		books2 = sortByReview(1000, body)
-		megaList2.append(books2)
+		body = extractStoryDetails(storyTitles, storyFooter)
+		books = sortByReview(minNumOfReviews, body)
+		megaList.append(books)
 
-	with open("harrypotter.txt", "w") as file:
-		for line in megaList2:
+	with open("fanfiction.txt", "w") as file:
+		for line in megaList:
 			file.write(str(line) + "\n")
-	return ''
+	return True
 
 
+def main():
+	print("-------------------FanfictionFinder----------------------", "\n\n\n\n\n")
+	nameOfFic = input("What's the title ? : ").replace(" ", "-")
+	while True:
+		try:
+			reviews = int(input("enter the minumum number of reviews allowed : "))
+			pages = int(input("how many pages should be searched through : "))
+			break
+		except ValueError:
+			print("please enter numbers")
+
+	if compileFics(nameOfFic, pages, reviews):
+		print("succesful")
+		return
+	else:
+		print("the title you inputed isn't quite correct")
+	
+if __name__ == "__main__":
+	main()
