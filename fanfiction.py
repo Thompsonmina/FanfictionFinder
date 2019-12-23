8334257d155d46a6e605724827d25c234420ec34
Thompson inc <froshiyke@gmail.com>
2019-12-23 22:28:36 +0100

    more improvements, writes prettier to the txt

--- fanfiction.py
+++ fanfiction.py
@@ -4,6 +4,49 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def main():
+	print("-------------------FanfictionFinder----------------------", "\n\n\n\n\n")
+	categories = {1: "book", 2: "anime", 3: "cartoons", 4: "movies", 5: "tv", 6: "games", 7: "plays"}
+	print(categories, "\n\n")
+	# displays fanfiction categories to choose from
+	while True:
+		try:
+			categoryNum = int(input("select a category by its number : "))
+			if not categoryNum in range(1, 8):
+				print("select a number provided")
+			else:	break
+		except:
+			print("you can only provide a digit only")
+
+	# get title of the fanfiction and ensure that the page is valid
+	while True:
+		nameOfFic = input("What's the title ? : ").replace(" ", "-")
+		restOfLink = f"{categories[categoryNum]}/{nameOfFic}"
+		if not titlePageExists(restOfLink):
+			print("the title you inputed isn't quite correct") 
+		else:
+			break
+
+	while True:
+		try:
+			reviews = int(input("enter the minumum number of reviews allowed : "))
+			pages = int(input("how many pages should be searched through : "))
+			break
+		except ValueError:
+			print("please enter numbers")
+
+	compileFics(restOfLink, pages, reviews)
+	print("succesful")
+
+
+def titlePageExists(partOfLink):
+	pageData = requests.get(f"https://www.fanfiction.net/{partOfLink}")
+	pageHtml = BeautifulSoup(pageData.text, "html.parser")
+	storyFooter = pageHtml.select(".z-padtop2")
+
+	if storyFooter:
+		return True
+	return False
 
 def extractStoryDetails(storyTitle, storyFooter):
 	"""Gets all the stories and thier links from a page"""
@@ -32,40 +75,28 @@ def compileFics(title, numOfPages,minNumOfReviews):
 	"""gets the html, processes it into a giant list  and writes that list to a txt file """
 	megaList = []
 	for i in range(1, numOfPages):
-		pageData = requests.get(f"https://www.fanfiction.net/book/{title}/?&srt=1&r=10&p={i}")
+		pageData = requests.get(f"https://www.fanfiction.net/{title}/?&srt=1&r=10&p={i}")
 		pageHtml = BeautifulSoup(pageData.text, "html.parser")
 		storyFooter = pageHtml.select(".z-padtop2")
 		storyTitles = pageHtml.select(".stitle")
-		
-		if not storyFooter:
-			return False
 
 		body = extractStoryDetails(storyTitles, storyFooter)
-		books = sortByReview(minNumOfReviews, body)
+		books = filterByReview(minNumOfReviews, body)
 		megaList.append(books)
+		writeFics(megaList)
+
+def writeFics(listOfFics):
 
 	with open("fanfiction.txt", "w") as file:
-		for line in megaList:
-			file.write(str(line) + "\n")
+		for line in listOfFics:
+			for stories in line:
+				file.write("title : " + stories["title"][0]  + "\n")
+				file.write("link : fanfiction.net" + stories["title"][1] + "\n")
+				file.write("reviews : " + str(stories["reviews"]) +"\n\n")
 	return True
 
 
-def main():
-	print("-------------------FanfictionFinder----------------------", "\n\n\n\n\n")
-	nameOfFic = input("What's the title ? : ").replace(" ", "-")
-	while True:
-		try:
-			reviews = int(input("enter the minumum number of reviews allowed : "))
-			pages = int(input("how many pages should be searched through : "))
-			break
-		except ValueError:
-			print("please enter numbers")
 
-	if compileFics(nameOfFic, pages, reviews):
-		print("succesful")
-		return
-	else:
-		print("the title you inputed isn't quite correct")
 	
 if __name__ == "__main__":
 	main()
